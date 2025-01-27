337a89a7e250fc10d5de177a8cec91d4291ef5f0
MvMukesh <mukeshmanral777@gmail.com>
2025-01-27 13:30:18 +0530

    updated requirements for local package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot GenAI',
+    version= '0.0.1',
+    author= 'Mukesh Manral',
+    author_email= 'mukeshmanral777@gmail.com',
+    packages= find_packages(),
+    install_requires = [])
